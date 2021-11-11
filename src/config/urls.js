2f66643cfecd34dafacb5086d210b20f7c7c71f7
sandycodebrew <sandeepdas@codebrewinnovations.com>
2021-11-11 17:28:51 +0530

    task detail screen

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,6 +1,6 @@
 //export const API_BASE_URL = 'https://api.royodispatch.com/api';
-export const API_BASE_URL = 'https://api.rdstaging.com/api';
-//export const API_BASE_URL = 'https://api.winhires.com/api';
+// export const API_BASE_URL = 'https://api.rdstaging.com/api';
+export const API_BASE_URL = 'https://api.winhires.com/api';
 
 // export const API_BASE_URL = 'http://192.168.103.73:8005';
 //export const API_BASE_URL = 'https://api.williamherrick.com/api';
