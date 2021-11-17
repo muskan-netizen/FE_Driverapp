306d35aa6f3b54b512d29b3e6267ebc65e4bf5bf
admin <pavan.s@codebrewinnovations.comgit config --global user.name Pavan Sharma>
2021-11-17 11:59:21 +0530

    camera picker added in task document screen

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,8 +1,8 @@
 //live
-export const API_BASE_URL = 'https://api.royodispatch.com/api';
+//export const API_BASE_URL = 'https://api.royodispatch.com/api';
 
 //Staging
-//export const API_BASE_URL = 'https://api.rdstaging.com/api';
+export const API_BASE_URL = 'https://api.rdstaging.com/api';
 
 //Developement
 // export const API_BASE_URL = 'https://api.winhires.com/api';
