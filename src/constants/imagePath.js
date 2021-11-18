41d1441cd6c3969333add7c49ec8218d6fe1bda9
admin <pavan.s@codebrewinnovations.comgit config --global user.name Pavan Sharma>
2021-11-18 12:54:55 +0530

    bug fixes

--- src/constants/imagePath.js
+++ src/constants/imagePath.js
@@ -21,7 +21,7 @@ export default {
   listMenu: require('../assets/images/ic_list.png'),
   locationOff: require('../assets/images/ic_location_off.png'),
   locationOn: require('../assets/images/ic_location_on.png'),
-  logout: require('../assets/images/ic_logout.png'),
+  logout: require('../assets/images/ic_Logout.png'),
   map: require('../assets/images/ic_map.png'),
   menu: require('../assets/images/ic_menu.png'),
   notesBlue: require('../assets/images/ic_notes_blue.png'),
@@ -74,5 +74,4 @@ export default {
   navigate: require('../assets/images/ic_navigate.png'),
   placeHolder: require('../assets/images/placeholder.png'),
   support2: require('../assets/images/ic_support_2.png'),
-
 };
