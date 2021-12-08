d4f629db2199113f858590b11af5de72818bb94b
surya narayan <suryanarayan@codebrewinnovations.com>
2021-12-08 13:06:41 +0530

    Drus and url

--- src/config/urls.js
+++ src/config/urls.js
@@ -2,7 +2,7 @@
 // export const API_BASE_URL = 'https://api.royodispatch.com/api';
 
 //Staging
- export const API_BASE_URL = 'https://api.rdstaging.com/api';
+//  export const API_BASE_URL = 'https://api.rdstaging.com/api';
 
 //tranzit app domain
 //export const API_BASE_URL = 'https://dispatch.tranzit.app/api';
@@ -19,6 +19,8 @@
 // export const API_BASE_URL = 'https://dispatch.cannabusxpress.com/api'
 // govachow
 // export const API_BASE_URL = 'https://dispatch.govachow.com/api'
+// africanize
+export const API_BASE_URL = 'https://dispatch.africanize.co/api'
 //Developement
 // export const API_BASE_URL = 'https://api.winhires.com/api';
 //export const API_BASE_URL = 'http://192.168.96.20:8010/api';
