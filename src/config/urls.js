a9eb35c5c070696fb64636c197cdb0c329fdc214
admin <pavan.s@codebrewinnovations.comgit config --global user.name Pavan Sharma>
2021-12-20 18:32:03 +0530

    bug fixes

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,8 +1,8 @@
 //live
-//export const API_BASE_URL = 'https://api.royodispatch.com/';
+export const API_BASE_URL = 'https://api.royodispatch.com/';
 
 //Staging
-export const API_BASE_URL = 'https://api.rdstaging.com/';
+//export const API_BASE_URL = 'https://api.rdstaging.com/';
 
 //export const API_BASE_URL = 'http://192.168.103.73:8006/'; preet sir local url
 
