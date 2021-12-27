9dd74d5cb86549d4cfa2aad5c85ea369f20e789c
admin <pavan.s@codebrewinnovations.comgit config --global user.name Pavan Sharma>
2021-12-27 11:23:32 +0530

    all latest code

--- src/Screens/DashBoard/DashBoard.js
+++ src/Screens/DashBoard/DashBoard.js
@@ -245,8 +245,8 @@ export default function DashBoard({route, navigation}) {
     () => fetchgentLogs(),
     userData && userData?.access_token
       ? userData?.team?.location_frequency
-        ? Number(userData?.team?.location_frequency) * 20000
-        : 20000
+        ? Number(userData?.team?.location_frequency) * 60000
+        : 60000
       : null,
   );
 
