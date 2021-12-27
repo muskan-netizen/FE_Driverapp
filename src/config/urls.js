c883b502c091902b764736f8c8c9d8e2c2c602c8
admin <pavan.s@codebrewinnovations.comgit config --global user.name Pavan Sharma>
2021-12-27 11:19:24 +0530

    all latest code

--- src/config/urls.js
+++ src/config/urls.js
@@ -2,7 +2,7 @@
 //export const API_BASE_URL = 'https://api.royodispatch.com/';
 
 //Staging
-//export const API_BASE_URL = 'https://api.rdstaging.com/';
+export const API_BASE_URL = 'https://api.rdstaging.com/';
 
 // export const API_BASE_URL = 'http://192.168.103.73:8006/'; // preet local url
 
@@ -64,7 +64,7 @@
 //export const API_BASE_URL = 'https://dispatcher.runrun.ae/';
 
 // drivree live domain
-export const API_BASE_URL = 'https://dispatcher.drivereeshops.com/';
+//export const API_BASE_URL = 'https://dispatcher.drivereeshops.com/';
 
 export const personaltoken = 'TMJdbQlNWkYl1JzMONzRgF4zQFuP8s';
 export const getApiUrl = endpoint => API_BASE_URL + endpoint;
