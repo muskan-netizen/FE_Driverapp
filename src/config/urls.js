a0db8c8c98a08ef0228353c79c00ed868e738061
surya narayan <suryanarayan@codebrewinnovations.com>
2022-01-04 14:18:03 +0530

    update the build

--- src/config/urls.js
+++ src/config/urls.js
@@ -50,7 +50,7 @@ export const API_BASE_URL = 'https://api.rdstaging.com/';
 // export const API_BASE_URL = 'http://192.168.103.73:800/';
 
 // destinationops app domain
-//export const API_BASE_URL = 'https://dispatch.destinationops.com/';
+// export const API_BASE_URL = 'https://dispatch.destinationops.com/';
 
 // somame live domain
 // export const API_BASE_URL = 'https://dispatch.somame-247.com/'
