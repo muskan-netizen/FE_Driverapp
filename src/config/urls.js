2f965892a39587bf37837a9594b06377ebcb237f
admin <pavan.s@codebrewinnovations.comgit config --global user.name Pavan Sharma>
2022-01-17 11:19:25 +0530

    donepacked splash icon changed

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,8 +1,8 @@
 //live
-//export const API_BASE_URL = 'https://api.royodispatch.com/';
+export const API_BASE_URL = 'https://api.royodispatch.com/';
 
 //Staging
-export const API_BASE_URL = 'https://api.rdstaging.com/';
+// export const API_BASE_URL = 'https://api.rdstaging.com/';
 
 // export const API_BASE_URL = 'http://192.168.103.73:8006/'; // preet local url
 // export const API_BASE_URL = 'http://192.168.103.73:800/';
@@ -44,7 +44,7 @@ export const API_BASE_URL = 'https://api.rdstaging.com/';
 // export const API_BASE_URL = 'https://grub.royodispatch.com/'
 
 // destinationops app domain
-// export const API_BASE_URL = 'https://dispatch.destinationops.com/';
+//export const API_BASE_URL = 'https://dispatch.destinationops.com/';
 
 // somame live domain
 // export const API_BASE_URL = 'https://dispatch.somame-247.com/'
