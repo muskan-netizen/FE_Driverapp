2c2047c18e1aa8d52a4d5516d00d495cc23f4e54
sandeep <sandeepdas@codebrewinnovations.com>
2022-01-25 13:00:24 +0530

    code merge with dev_sandy

--- src/config/urls.js
+++ src/config/urls.js
@@ -2,24 +2,17 @@
 export const API_BASE_URL = 'https://api.royodispatch.com/';
 
 //Staging
-<<<<<<< HEAD
-//export const API_BASE_URL = 'https://api.rdstaging.com/';
-=======
 // export const API_BASE_URL = 'https://api.rdstaging.com/';
->>>>>>> dev_sandy
 
 // export const API_BASE_URL = 'http://192.168.96.152:8006/'; // preet local url
 // export const API_BASE_URL = 'http://192.168.102.23:8005/'; // Gourav.s local url
 // export const API_BASE_URL = 'http://192.168.97.104:8010/'; // Sujata.s local url
 
-<<<<<<< HEAD
-=======
 
 //MARTINONWHEELS
 // export const API_BASE_URL = 'https://dispatch.martinionwheelsllc.org/';
 
 
->>>>>>> dev_sandy
 // VICI
 // export const API_BASE_URL = 'https://envios.vici.la/'
 
