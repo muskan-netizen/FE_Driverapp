7537c90aab87fe24aadb79ec7886bf149fbea250
pavan sharma <pavan.s@codebrewinnovations.com>
2022-01-27 11:58:23 +0530

    code upload with gova chow url

--- src/config/urls.js
+++ src/config/urls.js
@@ -2,7 +2,7 @@
 //export const API_BASE_URL = 'https://api.royodispatch.com/';
 
 //Staging
-export const API_BASE_URL = 'https://api.rdstaging.com/';
+//export const API_BASE_URL = 'https://api.rdstaging.com/';
 
 // export const API_BASE_URL = 'http://192.168.96.152:8006/'; // preet local url
 // export const API_BASE_URL = 'http://192.168.102.23:8005/'; // Gourav.s local url
@@ -29,7 +29,7 @@ export const API_BASE_URL = 'https://api.rdstaging.com/';
 // cannabusxpress
 // export const API_BASE_URL = 'https://dispatch.cannabusxpress.com/'
 // govachow
-// export const API_BASE_URL = 'https://dispatch.govachow.com/'
+export const API_BASE_URL = 'https://dispatch.govachow.com/';
 // africanize
 // export const API_BASE_URL = 'https://dispatch.africanize.co/'
 // orderchekout
