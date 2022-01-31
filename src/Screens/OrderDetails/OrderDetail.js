35960863884cbb3cb95ee6f53af2b2bf6812a05b
sandeep <sandeepdas@codebrewinnovations.com>
2022-01-31 10:48:29 +0530

    Remove edit order text function

--- src/Screens/OrderDetails/OrderDetail.js
+++ src/Screens/OrderDetails/OrderDetail.js
@@ -505,11 +505,11 @@ export default function OrderDetail({route, navigation}) {
         headerStyle={{backgroundColor: colors.white}}
         leftIcon={imagePath.backArrow}
         centerTitle={strings.ORDERDETAILS}
-        customRight={()=>(<TouchableOpacity
-          onPress={_onPressEditOrder}
-          >
-          <Text style={styles.editOrder}>{'Edit'}</Text>
-        </TouchableOpacity>)}
+        // customRight={()=>(<TouchableOpacity
+        //   onPress={_onPressEditOrder}
+        //   >
+        //   <Text style={styles.editOrder}>{'Edit'}</Text>
+        // </TouchableOpacity>)}
         // onPressLeft={() => navigation.toggleDrawer()}
         // hideRight={true}
         // customCenter={() => customCenter()}
