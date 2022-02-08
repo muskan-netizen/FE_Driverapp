2467d1ed32df1ac896119cbb812ea33f8d796133
pavan sharma <pavan.s@codebrewinnovations.com>
2022-02-08 11:09:31 +0530

    target added

--- src/config/urls.js
+++ src/config/urls.js
@@ -17,6 +17,12 @@ export const API_BASE_URL = 'https://api.royodispatch.com/';
 // VICI
 // export const API_BASE_URL = 'https://envios.vici.la/'
 
+//trucktirenow
+//export const API_BASE_URL = 'https://dispatch.trucktirenow.com/';
+
+//AgreeOnline
+//export const API_BASE_URL = 'https://dispatch.agrionline.ph/';
+
 // fleety staging domain
 //export const API_BASE_URL = 'https://fleety.rdstaging.com/';
 
