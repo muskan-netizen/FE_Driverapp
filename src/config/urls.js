cb0339c324db8a22f1fc1257213eaafeabb7a0eb
sandycodebrew <sandeepdas@codebrewinnovations.com>
2022-03-16 15:10:12 +0530

    Merged code with arun

--- src/config/urls.js
+++ src/config/urls.js
@@ -2,10 +2,10 @@
 // export const API_BASE_URL = 'https://api.royodispatch.com/';
 
 //Staging
-export const API_BASE_URL = 'https://api.rdstaging.com/';
+// export const API_BASE_URL = 'https://api.rdstaging.com/';
 
 //Developement
-//export const API_BASE_URL = 'https://api.winhires.com/';
+export const API_BASE_URL = 'https://api.winhires.com/';
 // export const API_BASE_URL = 'https://dispatcher.releezer.shop/';
 //UFOOD
 // export const API_BASE_URL = 'https://dispatch.ufood.ca/';
