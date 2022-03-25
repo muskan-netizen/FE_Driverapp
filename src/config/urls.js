ce57ba4b9b946430f2c029de7a80ab450c632151
pavan sharma <pavan.s@codebrewinnovations.com>
2022-03-25 10:44:57 +0530

    language added

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,8 +1,8 @@
 //live
-//export const API_BASE_URL = 'https://api.royodispatch.com/';
+export const API_BASE_URL = 'https://api.royodispatch.com/';
 
 //Staging
-export const API_BASE_URL = 'https://api.rdstaging.com/';
+//export const API_BASE_URL = 'https://api.rdstaging.com/';
 
 //Developement
 //export const API_BASE_URL = 'https://api.winhires.com/';
