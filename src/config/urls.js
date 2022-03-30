92430e572810df433612f97ce8c0885ce8921467
pavan sharma <pavan.s@codebrewinnovations.com>
2022-03-30 12:10:24 +0530

    bug dixes

--- src/config/urls.js
+++ src/config/urls.js
@@ -2,11 +2,11 @@
 //export const API_BASE_URL = 'https://api.royodispatch.com/';
 
 //Staging
-export const API_BASE_URL = 'https://api.rdstaging.com/';
+//export const API_BASE_URL = 'https://api.rdstaging.com/';
 
 //Developement
 //export const API_BASE_URL = 'https://api.winhires.com/';
-//export const API_BASE_URL = 'https://dispatcher.releezer.shop/';
+
 //UFOOD
 // export const API_BASE_URL = 'https://dispatch.ufood.ca/';
 // export const API_BASE_URL = 'http://192.168.96.152:8006/'; // preet local url
@@ -20,7 +20,7 @@ export const API_BASE_URL = 'https://api.rdstaging.com/';
 // export const API_BASE_URL = 'https://dispatch.hellodeliver.in/';
 
 //export const API_BASE_URL = 'https://pinkydeli.rdstaging.com/';
-//export const API_BASE_URL = 'https://dispatcher.releezer.shop';
+export const API_BASE_URL = 'https://dispatcher.releezer.shop/';
 
 //DOORSTEP
 // export const API_BASE_URL = 'https://dispatch.doorstep.sd/';
