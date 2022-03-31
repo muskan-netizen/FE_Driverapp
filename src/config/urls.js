c14ce8d13a6511d6f0d34fc5404fb136af1e37d3
Girraj <girraj.s@codebrewinnovations.com>
2022-03-31 12:11:06 +0530

    build version changes :

--- src/config/urls.js
+++ src/config/urls.js
@@ -2,7 +2,7 @@
 //export const API_BASE_URL = 'https://api.royodispatch.com/';
 
 //Staging
-export const API_BASE_URL = 'https://api.rdstaging.com/';
+// export const API_BASE_URL = 'https://api.rdstaging.com/';
 
 //Developement
 //export const API_BASE_URL = 'https://api.winhires.com/';
@@ -129,7 +129,7 @@ export const API_BASE_URL = 'https://api.rdstaging.com/';
 // export const API_BASE_URL = 'https://dispatcher.click-eat.com/'
 
 // halalhmc
-//export const API_BASE_URL = 'https://dispatcher.halalhmc.org/';
+export const API_BASE_URL = 'https://dispatcher.halalhmc.org/';
 // tranznet online
 // export const API_BASE_URL = 'https://dispatcher.tranzonline.com/'
 // runrun live domain
