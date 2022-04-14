da04cccf80fb973bfcc9bc87ed7be3a2140ca201
Girraj Sharma <girraj.s@codebrewinnovations.com>
2022-04-14 19:04:18 +0530

    firebase file changes

--- src/config/urls.js
+++ src/config/urls.js
@@ -2,10 +2,13 @@
 // export const API_BASE_URL = 'https://api.royodispatch.com/';
 
 //Staging
-export const API_BASE_URL = 'https://api.rdstaging.com/';
+// export const API_BASE_URL = 'https://api.rdstaging.com/';
 
 //YogoFood
 // export const API_BASE_URL = 'https://yogofood-dispatch.netsolutionindia.com/';
+
+//Dlvrd
+export const API_BASE_URL = 'https://dlvrd-dispatch.netsolutionindia.com/';
 
 //Developement
 //export const API_BASE_URL = 'https://api.winhires.com/';
