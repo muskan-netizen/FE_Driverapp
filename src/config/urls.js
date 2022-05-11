4ce4687cec0e203929d1866d6dcfaefcdcac5f5e
CBL-Mac-001 <cbl-mac-001@CBL-Mac-001s-iMac-Pro-2.local>
2022-05-11 13:14:02 +0530

    bug fixes

--- src/config/urls.js
+++ src/config/urls.js
@@ -2,10 +2,10 @@
 // export const API_BASE_URL = 'https://api.royodispatch.com/';
 
 //Staging
-// export const API_BASE_URL = 'https://api.rdstaging.com/';
+export const API_BASE_URL = 'https://api.rdstaging.com/';
 
 
-export const API_BASE_URL ="https://dispatch.ipickndrop.com/"
+// export const API_BASE_URL ="https://dispatch.ipickndrop.com/"
 
 //PeerDeliveries
 // export const API_BASE_URL ='https://dispatch.peerdeliveries.com/'
