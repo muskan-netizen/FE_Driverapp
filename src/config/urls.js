924fa7b72b299bad7320341ba239d325a8e42da7
CBL-Mac-033 <cbl-mac-033@CBL-Mac-033s-iMac-Pro.local>
2022-06-15 17:09:19 +0530

    build

--- src/config/urls.js
+++ src/config/urls.js
@@ -2,7 +2,7 @@
 //  export const API_BASE_URL = 'https://api.royodispatch.com/';
 
 //Staging
-export const API_BASE_URL = 'https://api.rdstaging.com/';
+// export const API_BASE_URL = 'https://api.rdstaging.com/';
 // export const API_BASE_URL = 'https://api.rdstaging.com/';
 //export const API_BASE_URL = 'https://dispatch.carlitoo.com/';
 
@@ -240,7 +240,7 @@ export const API_BASE_URL = 'https://api.rdstaging.com/';
 // export const API_BASE_URL = 'https://dispatch.helpnowrightnow.com/';
 
 // tranzit live domain
-// export const API_BASE_URL = 'https://dispatch.tranzit.app/';
+export const API_BASE_URL = 'https://dispatch.tranzit.app/';
 // africanvillage
 // export const API_BASE_URL = 'https://dispatch.africanvillage.market/'
 // swiftandvalu
