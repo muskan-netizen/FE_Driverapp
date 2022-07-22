fe033fe5bdcbe0c5f1f63baa5ef1419490acd909
CBL-Mac-030 <cbl-mac-030@CBL-Mac-030s-iMac-Pro.local>
2022-07-22 16:29:57 +0530

    target

--- src/Screens/ShortCode/ShortCode.js
+++ src/Screens/ShortCode/ShortCode.js
@@ -2110,7 +2110,7 @@ export default function ShortCode({route, navigation}) {
 
       let updatedShortCode = shortCode;
       // updatedShortCode = 'a5c8d0';
-      //  let updatedShortCode = 'b077a9';
+      //  let updatedShortCode = '6755bb';
 
       actions
         .initApp({shortCode: updatedShortCode}, header)
