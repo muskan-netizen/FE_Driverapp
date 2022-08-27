d1fac098e81484020635c36c190b734f7f2df723
CBL-Mac-001 <cbl-mac-001@CBL-Mac-001s-iMac-Pro-2.local>
2022-08-27 17:25:21 +0530

    bug fixes

--- src/config/urls.js
+++ src/config/urls.js
@@ -11,14 +11,14 @@
 // export const API_BASE_URL = 'https://zest.royodispatch.com/';
 
 // export const API_BASE_URL='https://dispatch.tmgsalesllc.com/';
-// export const API_BASE_URL='http://192.168.96.84:9091/'
+// export const API_BASE_URL='https://dispatch.aaujaaunepal.com/'
 //Staging
  export const API_BASE_URL = 'https://api.rdstaging.com/';
  
 // export const API_BASE_URL = 'https://api.winhires.com/';
 // export const API_BASE_URL = 'https://api.rdstaging.com/';
 //export const API_BASE_URL = 'https://dispatch.carlitoo.com/';
-// export const API_BASE_URL = 'http://192.168.96.152:8006/';
+// export const API_BASE_URL = 'http://192.168.96.84:9091/';
  //export const API_BASE_URL = 'https://api.royodispatch.com/';
 
 
