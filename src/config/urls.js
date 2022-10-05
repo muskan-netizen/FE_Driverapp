66575bd786fb9612e410c37a06b86fcec3d4927d
CBL-MAC-016 <cbl-mac-016@CBL-MAC-016s-Mac-mini.local>
2022-10-05 15:40:34 +0530

    changes

--- src/config/urls.js
+++ src/config/urls.js
@@ -3,8 +3,8 @@
 //  export const API_BASE_URL = 'https://api.royodispatch.com/';
 // export const API_BASE_URL = 'https://sales.royodispatch.com/';
 //Staging
-// export const API_BASE_URL = 'https://api.rdstaging.com/';
-export const API_BASE_URL = 'https://spryton.royodispatch.com/';
+export const API_BASE_URL = 'https://api.rdstaging.com/';
+// export const API_BASE_URL = 'https://spryton.royodispatch.com/';
 // export const API_BASE_URL = 'https://dispatch.halatalabat.com/'
 
 //  export const API_BASE_URL = 'https://dispatch.ubercann.com/';
