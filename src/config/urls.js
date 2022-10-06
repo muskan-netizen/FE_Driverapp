22ea62ff62e0816a99409c96428668659e06a635
CBL-MAC-034 <cbl-mac-034@CBL-MAC-034s-iMac-Pro.local>
2022-10-06 12:52:58 +0530

    Build

--- src/config/urls.js
+++ src/config/urls.js
@@ -15,7 +15,7 @@
 
 // export const API_BASE_URL='https://dispatch.tmgsalesllc.com/';
 //Staging
-//  export const API_BASE_URL = 'https://api.rdstaging.com/';
+ export const API_BASE_URL = 'https://api.rdstaging.com/';
 
 // export const API_BASE_URL = 'https://api.rdstaging.com/';
 
@@ -53,7 +53,7 @@
 //Papruki
 // export const API_BASE_URL='https://dispatch.papiruki.com/'
 // export const API_BASE_URL = 'https://dispatch.drusdeliveries.com/';
-export const API_BASE_URL='https://dispatch.dbairro.com/'
+// export const API_BASE_URL='https://dispatch.dbairro.com/'
 // export const API_BASE_URL = 'https://dispatch.pas41.com/'
 
 // tmg Shops
