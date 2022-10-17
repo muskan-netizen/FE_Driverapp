3b06ead58eb62385b4b7a786b0fadaa73357c8ea
CBL-MAC-034 <cbl-mac-034@CBL-MAC-034s-iMac-Pro.local>
2022-10-17 15:42:08 +0530

    builds

--- src/config/urls.js
+++ src/config/urls.js
@@ -3,11 +3,11 @@
  //export const API_BASE_URL = 'https://api.royodispatch.com/';
 // export const API_BASE_URL = 'https://sales.royodispatch.com/';
 //Staging
-export const API_BASE_URL = 'https://api.rdstaging.com/';
+// export const API_BASE_URL = 'https://api.rdstaging.com/';
 // export const API_BASE_URL='https://delivery.rdstaging.com/'
 // export const API_BASE_URL = 'https://spryton.royodispatch.com/';
 // export const API_BASE_URL = 'https://dispatch.halatalabat.com/'
-// export const API_BASE_URL='https://dispatch.sheryders.com/'
+export const API_BASE_URL='https://dispatch.sheryders.com/'
 //  export const API_BASE_URL = 'https://dispatch.ubercann.com/';
 // export const API_BASE_URL = 'https://dispatch.swantaxi.net/';
 // export const API_BASE_URL = 'https://dispatch.keystonedelivery.com/'
