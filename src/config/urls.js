ce7ab5bcc5b09b509d85adadd947c294ad500f96
CBL-MAC-034 <cbl-mac-034@CBL-MAC-034s-iMac-Pro.local>
2022-11-15 12:45:51 +0530

    target And otp

--- src/config/urls.js
+++ src/config/urls.js
@@ -5,7 +5,7 @@
 // //Staging
 //  export const API_BASE_URL = 'https://api.rdstaging.com/';
 //  export const API_BASE_URL='https://zuzucleans.royodispatch.com/'
-
+// export const API_BASE_URL='http://192.168.100.142:8001/'
 // export const API_BASE_URL='https://dispatch.bendeta.biz/'
 // export const API_BASE_URL='http://192.168.100.130:9091/'
 //export const API_BASE_URL   ='https://fcba-103-72-170-243.ngrok.io/'
