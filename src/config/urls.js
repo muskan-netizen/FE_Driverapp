968296ba1eb1b2cfc49d6e872fa4ccd99d5c5dba
CBL-Mac-033 <cbl-mac-033@CBL-Mac-033s-iMac-Pro.local>
2022-12-08 18:59:39 +0530

    biulds

--- src/config/urls.js
+++ src/config/urls.js
@@ -4,16 +4,18 @@
 // export const API_BASE_URL = 'https://sales.royodispatch.com/';
 // export const API_BASE_URL='http://192.168.101.140:9091/'
 // //Staging
-export const API_BASE_URL = 'https://api.rdstaging.com/';
+// export const API_BASE_URL = 'https://api.rdstaging.com/';
 
 // export const API_BASE_URL = 'https://dispatch.dimahalal.com/'
-export const API_BASE_URL = 'https://dispatch.laundrozone.net/';
+// export const API_BASE_URL = 'https://dispatch.laundrozone.net/';
 
 //  export const API_BASE_URL='https://zuzucleans.royodispatch.com/'
 // export const API_BASE_URL='http://192.168.100.142:8001/'
 // export const API_BASE_URL = 'https://dispatch.bendeta.biz/';
 // export const API_BASE_URL = 'https://dispatch.shophairrun.app/';
 // export const API_BASE_URL='http://192.168.100.130:9091/'
+
+ export const API_BASE_URL='https://dispatch.movingwheelsdeliveryservices.com/'
 
 //export const API_BASE_URL   ='https://fcba-103-72-170-243.ngrok.io/'
 // export const API_BASE_URL='https://delivery.rdstaging.com/'
