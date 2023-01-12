5167d02e54bb3644b9f29870b94b22904a83fcc3
dilawer18 <dilawer1807948@gmail.com>
2023-01-12 18:01:55 +0530

    mesia

--- src/constants/lang/index.js
+++ src/constants/lang/index.js
@@ -1,10 +1,12 @@
 import LocalizedStrings from 'react-native-localization';
+import { appIds } from '../../utils/constants/DynamicAppKeys';
 
 import ar from './ar';
 import de from './de';
 import en from './en';
 import es from './es';
 import fr from './fr';
+import heb from './heb';
 import ne from './ne';
 import pr from './pr';
 import ptBr from './ptBr';
@@ -15,9 +17,20 @@ import tr from './tr';
 import vi from './vi';
 import zh from './zh';
 
+
+
+const arbicFile = () => {
+  switch (DeviceInfo.getBundleId()) {
+    case appIds?.viralClean:
+      return heb;
+    default:
+      return ar;
+  }
+};
+
 let strings = new LocalizedStrings({
   en: en,
-  ar: ar,
+  ar: arbicFile(),
   es: es,
   de: de,
   fr: fr,
