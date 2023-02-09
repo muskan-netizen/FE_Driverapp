c5d18be130abb40c21f99f2ec27cac81cf61d151
pavan sharma <pavanpancholi8@gmail.com>
2023-02-09 17:25:39 +0530

    latest code

--- src/config/urls.js
+++ src/config/urls.js
@@ -11,9 +11,9 @@
 // export const API_BASE_URL='http://192.168.101.140:9091/'
 // export const API_BASE_URL='https://dispatch.usemeow.com/'
 // //Staging
-export const API_BASE_URL='https://dispatch.uber-weeds.com/'
+// export const API_BASE_URL='https://dispatch.uber-weeds.com/'
 // export const API_BASE_URL='https://dispatch.vduonline.co.in/'
-// export const API_BASE_URL = 'https://api.rdstaging.com/';
+ export const API_BASE_URL = 'https://api.rdstaging.com/';
 // export const API_BASE_URL='https://dispatch.nittosadai.com/'
 //  export const API_BASE_URL='https://zuzucleans.royodispatch.com/'
 // export const API_BASE_URL='https://delivery.go912.app/'
