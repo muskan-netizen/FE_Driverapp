816bcdfc76550d6ff11b77f6c75f4ed169d564a9
CBL-MAC-016 <cbl-mac-016@CBL-MAC-016s-Mac-mini.local>
2023-02-14 12:11:59 +0530

    builds

--- src/config/urls.js
+++ src/config/urls.js
@@ -26,7 +26,7 @@
 // export const API_BASE_URL='https://dispatch.vduonline.co.in/'
 //  export const API_BASE_URL = 'https://api.rdstaging.com/';
 //  export const API_BASE_URL = 'https://dispatch.destinationops.com/';
- export const API_BASE_URL = 'https://api.rdstaging.com/';
+//  export const API_BASE_URL = 'https://api.rdstaging.com/';
  // export const API_BASE_URL='https://dispatch.vduonline.co.in/'
 // export const API_BASE_URL='https://dispatch.nittosadai.com/'
 //  export const API_BASE_URL='https://zuzucleans.royodispatch.com/'
@@ -501,7 +501,7 @@
 // hiperabasto
 // export const API_BASE_URL = 'https://dispatch.hiperabasto.mx/';
 //parcelworksdelivery
-// export const API_BASE_URL ='https://dispatch.parcelworksdelivery.com/';
+export const API_BASE_URL ='https://dispatch.parcelworksdelivery.com/';
 // amwer 
 
 // export const API_BASE_URL='https://dispatch.awamerapp.com/'
