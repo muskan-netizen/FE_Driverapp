57979c3ad05da494517ec5fbcb2d2ce874bbfc1b
pavan sharma <pavanpancholi8@gmail.com>
2023-02-21 14:49:29 +0530

    issue fixed for bide and ride

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ messaging().setBackgroundMessageHandler(async remoteMessage => {
       } else {
         console.log(' hey i am here');
         actions.isModalVisibleForAcceptReject({
-          isModalVisibleForAcceptReject: true,
+          isModalVisibleForAcceptReject: notificationType =='bid_ride_request' ?false : true,
           notificationData: remoteMessage,
         });
       }
