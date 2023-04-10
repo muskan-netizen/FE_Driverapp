0c3c77085b8103c50d010ca7af5a9086b95d65d2
pavan sharma <pavanpancholi8@gmail.com>
2023-04-10 18:16:47 +0530

    locate assets updated

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,4 +1,4 @@
-// export const API_BASE_URL = 'https://api.rdstaging.com/';
+ export const API_BASE_URL = 'https://api.rdstaging.com/';
 // export const API_BASE_URL='https://dispatch.jeevannapp.com/'
 //export const API_BASE_URL = 'https://805c-103-72-170-243.ngrok.io/';
 // export const API_BASE_URL = 'https://dispatch.shelf.ae';
@@ -144,7 +144,7 @@
 // export const API_BASE_URL = ' https://dispatch.halatalabat.com/'
 
 // brilApp
-export const API_BASE_URL = 'https://dispatch.brilapp.com/'
+// export const API_BASE_URL = 'https://dispatch.brilapp.com/'
 
 //amazingTaxi
 // export const API_BASE_URL = 'https://dispatch.rhondaisamazingtaxi.com/'
