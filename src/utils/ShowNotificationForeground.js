d9b64aca803b4922f707bb511ecf6d8e60cba49d
pavan sharma <pavanpancholi8@gmail.com>
2023-04-12 18:40:59 +0530

    marge with latest code

--- src/utils/ShowNotificationForeground.js
+++ src/utils/ShowNotificationForeground.js
@@ -1,3 +1,89 @@
+// import { useEffect } from 'react';
+// import { Platform } from 'react-native';
+// import PushNotificationIOS from '@react-native-community/push-notification-ios';
+// import messaging from '@react-native-firebase/messaging';
+// // import PushNotification, { Importance } from 'react-native-push-notification';
+// import actions from '../redux/actions';
+// import { navigate } from '../navigation/NavigationService';
+// import navigationStrings from '../navigation/navigationStrings';
+// import notifee, { AndroidColor, AndroidImportance } from '@notifee/react-native';
+
+// const ShowNotificationForeground = props => {
+//   useEffect(() => {
+//     const unsubscribe = messaging().onMessage(async remoteMessage => {
+//       console.log('remote message foreground', remoteMessage);
+//       const {data, messageId, notification} = remoteMessage;
+//       console.log(remoteMessage.data, notification, 'datadatadatadata');
+//       let notificationType = data?.type || data?.notificationType || 'AR';
+//       {
+//         Platform.OS == 'ios'
+//           ? PushNotificationIOS.addNotificationRequest({
+//               id: messageId,
+//               body: data?.message || '',
+//               title: data?.message || '',
+//               sound:
+//                 notification.sound == 'notification.mp3'
+//                   ? 'notification.mp3'
+//                   : 'default',
+//             })
+//           : PushNotification.localNotification({
+//               channelId: notification.android.channelId,
+//               id: messageId,
+//               body: data?.message || '',
+//               title: data?.message || '',
+//               soundName: notification.android.sound,
+//               vibrate: true,
+//               playSound: true,
+//             });
+//       }
+
+//       if (data.data == 'chat_text') {
+//         PushNotification.localNotification({
+//           message: data?.body,
+//         });
+//       }
+
+//       if (
+//         Platform.OS == 'android' &&
+//         notification.android.sound == 'notification'
+//       ) {
+//         console.log('here>>2');
+//         if (!!data && !!notificationType && notificationType != 'N') {
+//           console.log(notificationType, 'notificationTypenotificationType');
+//           actions.isModalVisibleForAcceptReject({
+//             isModalVisibleForAcceptReject:notificationType =='bid_ride_request'?false: true,
+//             notificationData: remoteMessage,
+//           });
+//         }
+//         if (data?.callback_url != '' && data?.callback_url != null) {
+//           navigate(navigationStrings.ORDERDETAIL, {
+//             data: { item: data?.callback_url, fromNotification: true },
+//           });
+//         }
+//       }
+//       if (Platform.OS == 'ios' && notification.sound == 'notification.mp3') {
+//         console.log('here>>3');
+//         if (data && notificationType && notificationType != 'N') {
+//           actions.isModalVisibleForAcceptReject({
+//             isModalVisibleForAcceptReject:notificationType =='bid_ride_request' ?false : true,
+//             notificationData: remoteMessage,
+//           });
+//         }
+//         if (data?.callback_url != '' && data?.callback_url != null) {
+//           navigate(navigationStrings.ORDERDETAIL, {
+//             data: { item: data?.callback_url, fromNotification: true },
+//           });
+//         }
+//       }
+//     });
+//     return unsubscribe;
+//   }, []);
+//   return null;
+// };
+
+// export default ShowNotificationForeground;
+
+
 import { useEffect } from 'react';
 import { Platform } from 'react-native';
 import PushNotificationIOS from '@react-native-community/push-notification-ios';
@@ -12,44 +98,63 @@ const ShowNotificationForeground = props => {
   useEffect(() => {
     const unsubscribe = messaging().onMessage(async remoteMessage => {
       console.log('remote message foreground', remoteMessage);
-      const {data, messageId, notification} = remoteMessage;
-      console.log(remoteMessage.data, notification, 'datadatadatadata');
+      const { data, messageId, notification } = remoteMessage;
       let notificationType = data?.type || data?.notificationType || 'AR';
-      {
-        Platform.OS == 'ios'
-          ? PushNotificationIOS.addNotificationRequest({
-              id: messageId,
-              body: data?.message || '',
-              title: data?.message || '',
-              sound:
-                notification.sound == 'notification.mp3'
-                  ? 'notification.mp3'
-                  : 'default',
-            })
-          : PushNotification.localNotification({
-              channelId: notification.android.channelId,
-              id: messageId,
-              body: data?.message || '',
-              title: data?.message || '',
-              soundName: notification.android.sound,
-              vibrate: true,
-              playSound: true,
-            });
+
+      const channelId = await notifee.createChannel({
+        id: 'default-channel-id',
+        name: 'Default Channel',
+        vibration: true,
+        lightColor: AndroidColor.YELLOW,
+        sound: 'default',
+        importance: AndroidImportance.HIGH,
+      });
+      const channelIdRoyo = await notifee.createChannel({
+        id: 'Royo-Delivery',
+        name: 'Royo Delivery',
+        vibration: true,
+        lightColor: AndroidColor.YELLOW,
+        sound: 'notification',
+        importance: AndroidImportance.HIGH,
+      });
+      let displayNotificationData = {}
+      if (Platform.OS == "ios") {
+        displayNotificationData = {
+          title: notificationType || notification?.title || '',
+          body: data?.message || notification?.body || '',
+          data: { ...data },
+        };
+
+      }
+      else {
+        displayNotificationData = {
+          title: notificationType || notification?.title || '',
+          body: data?.message || notification?.body || '',
+          android: {
+            sound: notification.sound == 'notification'
+              ? 'notification'
+              : 'default',
+            channelId: notification.android?.channelId || channelId,
+            pressAction: {
+              id: 'default',
+            },
+            importance: AndroidImportance.HIGH,
+
+          },
+          data: { ...data },
+        };
       }
 
-      if (data.data == 'chat_text') {
-        PushNotification.localNotification({
-          message: data?.body,
-        });
-      }
+      console.log(displayNotificationData, "displayNotificationData.>>>>>")
+
+      await notifee.displayNotification(displayNotificationData);
 
       if (
         Platform.OS == 'android' &&
         notification.android.sound == 'notification'
       ) {
         console.log('here>>2');
-        if (!!data && !!notificationType && notificationType != 'N') {
-          console.log(notificationType, 'notificationTypenotificationType');
+        if (data && notificationType && notificationType != 'N') {
           actions.isModalVisibleForAcceptReject({
             isModalVisibleForAcceptReject:notificationType =='bid_ride_request'?false: true,
             notificationData: remoteMessage,
