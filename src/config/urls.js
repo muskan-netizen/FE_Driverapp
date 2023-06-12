a1b09f2f8a549a159b2964b10c6da688b371a8f9
Vivek <vivek.s@codebrewinnovations.com>
2023-06-12 19:28:26 +0530

    url

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://sales.rdstaging.com/';
+export const API_BASE_URL = 'https://api.rdstaging.com/';
 // export const API_BASE_URL='https://dispatch.jeevannapp.com/'
 //export const API_BASE_URL = 'https://805c-103-72-170-243.ngrok.io/';
 // export const API_BASE_URL = 'https://dispatch.shelf.ae';
