02ed1760c83c6ad7d896240e3387c7cdc84e5f61
brew-arslan <arslan.khan@codebrewinnovations.com>
2024-09-06 14:47:57 +0530

    cla_customization

--- src/config/urls.js
+++ src/config/urls.js
@@ -2,7 +2,7 @@
 // export const API_BASE_URL = 'https://dispatch.n2gobeauty.com/';
 //export const API_BASE_URL = 'https://805c-103-72-170-243.ngrok.io/';
 
-export const API_BASE_URL = 'https://api.royodispatch.com/'
+export const API_BASE_URL = 'https://cla.rdstaging.com/'
 
 // export const API_BASE_URL = 'https://grub.rdstaging.com/';
 
--- src/utils/constants/DynamicAppKeys.js
+++ src/utils/constants/DynamicAppKeys.js
@@ -1,14 +1,22 @@
 import { Platform } from 'react-native';
+import { getBundleId } from 'react-native-device-info';
 
 const shortCodes = {
-  ace: '3c58a1'
+  cla:'cf0b96'
 };
 
 const appIds = {
-  ace: Platform.select({
-    ios: 'com.Ace.Dispatch',
-    android: 'com.Ace.royodispatcher',
-  }),  
+  cla: Platform.select({
+    ios: 'com.royodispatcher.cla',
+    android: 'com.royodispatcher.cla',
+  }),
 };
 
-export { appIds, shortCodes };
+const socialKeys = {
+  TWITTER_COMSUMER_KEY:
+    'R66DHARfuoYAPowApUxNxwbPi',
+  TWITTER_CONSUMER_SECRET:
+    'itcicJ7fUV3b73B8V05GEDBo4tzxGox2Si2q0BCk5pue327k15',
+};
+
+export { appIds, socialKeys, shortCodes };
